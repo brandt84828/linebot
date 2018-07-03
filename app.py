9cd3705c0b101cc9aa5fb9107ad377ea81d81b46
brandt84828 <brandt84828@gmail.com>
2018-07-03 12:04:01 +0800

    make it better

--- app.py
+++ app.py
@@ -45,9 +45,10 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
      if event.message.text == "news":
+        message = TextSendMessage(text='ABCDE')
         line_bot_api.reply_message(
             event.reply_token,
-            'ABCDE')
+            message)
      else:
         message = TextSendMessage(text=event.message.text)#event.message.text
         line_bot_api.reply_message(
