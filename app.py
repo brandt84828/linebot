7b511a3b3f2e2dd9b97d575eadeeb642d5816cc4
brandt84828 <brandt84828@gmail.com>
2018-07-03 12:00:02 +0800

    make it better

--- app.py
+++ app.py
@@ -42,32 +42,18 @@ def callback():
 
     return 'OK'
 
-def apple_news():
-    target_url = 'https://tw.appledaily.com/new/realtime'
-    print('Start parsing appleNews....')
-    rs = requests.session()
-    res = rs.get(target_url, verify=False)
-    soup = BeautifulSoup(res.text, 'html.parser')
-    content = ""
-    for index, data in enumerate(soup.select('.rtddt a'), 0):
-        if index == 5:
-            return content
-        link = data['href']
-        content += '{}\n\n'.format(link)
-    return content
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)#event.message.text
-    line_bot_api.reply_message(
-        event.reply_token,
-        message)
-    if event.message.text == "蘋果即時新聞":
-        content = apple_news()
+     if event.message.text == "news":
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text=content))
-        return 0
+            'ABCDE')
+     else:
+        message = TextSendMessage(text=event.message.text)#event.message.text
+        line_bot_api.reply_message(
+            event.reply_token,
+            message)
+       
 
 import os
 if __name__ == "__main__":
